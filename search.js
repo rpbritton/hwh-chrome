75415f041a381a5be13cc14fc2e6ff293cf6b8ae
ryan <rpbritton@outlook.com>
2017-09-28 23:01:06 -0400

    somewhat complete search box

---
+++ search.js
@@ -0,0 +1,6 @@
+/*var searchButton = document.getElementById("search-button");
+searchButton.addEventListener("mousedown", function(ev) {
+	inkAnimation(ev, this, "#b8b8b8");
+});
+
+searchButton.addEventListener("click", function(ev) {});*/
--- sidebar.js
+++ sidebar.js
@@ -1,6 +1,6 @@
-var sidebarButtons = document.getElementsByClassName('sidebar-button');
-var selectedButton = sidebarButtons[0];
-selectButton(selectedButton); // default selected button
+var sidebarButtons = document.getElementsByClassName("sidebar-button");
+var selectedButton = sidebarButtons[1];		// default selected button
+selectButton(selectedButton);
 
 for (var x = 0; x < sidebarButtons.length; x++) {
 	sidebarButtons[x].addEventListener("mousedown", function(ev) {
@@ -16,6 +16,8 @@ function selectButton(el) {
 	selectedButton.firstElementChild.style.color = "#b8b8b8";
 	selectedButton = el;
 
-	document.getElementById('sidebar-selected').style.top = el.offsetTop +"px";
+	document.getElementById("sidebar-selected").style.top = el.offsetTop +"px";
 	el.firstElementChild.style.color = "#f8f8f8";
 }
+
+document.getElementById("sidebar").style.transform = "translate(0, 0)";
