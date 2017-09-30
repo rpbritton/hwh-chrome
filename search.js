e6a7115a63ac88c0d082910119d4667d9251ae08
ryan <rpbritton@outlook.com>
2017-09-29 22:43:57 -0400

    search bar functionality

--- search.js
+++ search.js
@@ -1,6 +1,37 @@
-/*var searchButton = document.getElementById("search-button");
-searchButton.addEventListener("mousedown", function(ev) {
+var search = document.getElementById("search");
+search.addEventListener("mousedown", function(ev) {
 	inkAnimation(ev, this, "#b8b8b8");
 });
 
-searchButton.addEventListener("click", function(ev) {});*/
+var searchBox = document.getElementById("search-box");
+searchBox.addEventListener("focus", function(ev) {
+	searchIcon.style.color = "#b8b8b8";
+});
+searchBox.addEventListener("focusout", function(ev) {
+	if (!searchBox.value) {
+		searchIcon.style.color = "";
+	}
+});
+
+searchBox.addEventListener("input", function(ev) {
+	if (searchBox.value.substring(searchBox.value.length-1) == "\n") {
+		searchBox.value = searchBox.value.substring(0, searchBox.value.length-1);
+	}
+	if (!searchBox.value) {
+		searchClear.style.opacity = "";
+	}
+	else {
+		searchClear.style.opacity = "1";
+	}
+});
+
+var searchIcon = document.getElementById("search-icon");
+search.addEventListener("click", function(ev) {
+	searchBox.focus();
+});
+
+var searchClear = document.getElementById("search-clear");
+searchClear.addEventListener("click", function(ev) {
+	searchBox.value = "";
+	searchClear.style.opacity = "";
+});
