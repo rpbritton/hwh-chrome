72dbf3cd956b39c34aa1863d186ed95e8f973f0e
ryan <rpbritton@outlook.com>
2017-10-01 22:49:57 -0400

    better auth

--- pageUser.js
+++ pageUser.js
@@ -1,59 +1,41 @@
-/*
-chrome.identity.getAuthToken({"interactive": true}, function(oauth2Token) {
-	console.log(oauth2Token);
-//	"https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=" +oauth2Token
-//	chrome.identity.removeCachedAuthToken({ token: oauth2Token }, function() {
-//		console.log("token removed");
-//	});
-//	"https://accounts.google.com/o/oauth2/revoke?token=" +oauth2Token
-});
-//*/
-//console.log(chrome.identity.getRedirectURL());
-/*
-// Using chrome.identity
 var manifest = chrome.runtime.getManifest();
+var oauthUrl = "https://accounts.google.com/o/oauth2/auth?response_type=token";
+oauthUrl += "&client_id=" +encodeURIComponent(manifest.oauth2.client_id);
+oauthUrl += "&scope=" +encodeURIComponent(manifest.oauth2.scopes.join(" "));
+oauthUrl += "&redirect_uri=" +chrome.identity.getRedirectURL("oauth2");
 
-var clientId = encodeURIComponent(manifest.oauth2.client_id);
-var scopes = encodeURIComponent(manifest.oauth2.scopes.join(' '));
-var redirectUri = encodeURIComponent('https://' + chrome.runtime.id + '.chromiumapp.org');
+function addAccount() {
+	chrome.identity.launchWebAuthFlow({"url": oauthUrl +"&prompt=select_account", "interactive": true}, function(promise) {
+		if (chrome.runtime.lastError) {
+			console.log(chrome.runtime.lastError.message);
+		}
+		else {
+			var token = promise.substring(promise.indexOf("#access_token=") +14, promise.indexOf("&token_type="));
+			
+			var request = new XMLHttpRequest();
+			request.onreadystatechange = function() {
+				if (this.status >= 200 && this.status < 400 && this.readyState == 4) {
+					var resp = JSON.parse(request.responseText);
+					console.log(resp);
+					getToken(resp.id);
+				}
+			};
 
-var url = 'https://accounts.google.com/o/oauth2/auth' + 
-		'?response_type=token&client_id=' + clientId
-		+ '&scope=' + scopes
-//		+ '&approval_prompt=force'
-//		+ '&access_type=offline'
-		+ '&redirect_uri=' + chrome.identity.getRedirectURL("oauth2");
-console.log(url);
+			request.open("GET", "https://www.googleapis.com/oauth2/v1/userinfo?access_token=" +token, true);
+			request.send();
+		}
+	});
+}
 
-chrome.identity.launchWebAuthFlow({
-        'url': url, 
-        'interactive': true
-    }, 
-    function(redirectedTo) {
-        if (chrome.runtime.lastError) {
-            // Example: Authorization page could not be loaded.
-            console.log(chrome.runtime.lastError.message);
-        }
-        else {
-//            var response = redirectedTo.split('#', 2)[1];
-
-            // Example: id_token=<YOUR_BELOVED_ID_TOKEN>&authuser=0&hd=<SOME.DOMAIN.PL>&session_state=<SESSION_SATE>&prompt=<PROMPT>
-//            console.log(response);
-		console.log(redirectedTo);
-		url += '&login_hint=mr.ryan.britton@gmail.com';
-		chrome.identity.launchWebAuthFlow({
-			'url': url,
-			'interactive': true
-		},
-		function(newRedirectTest) {
-		console.log(newRedirectTest);
-		});
-//		chrome.identity.removeCachedAuthToken(redirectedTo.substring(redirectedTo.indexOf, function() {
-//			console.log("token removed");
-//		});
-        }
-    }
-);
-//*/
-
-
+function getToken(userId) {
+	chrome.identity.launchWebAuthFlow({"url": oauthUrl +"&login_hint=" +userId, "interactive": true}, function(promise) {
+		if (chrome.runtime.lastError) {
+			console.log(chrome.runtime.lastError.message);
+		}
+		else {
+			var token = promise.substring(promise.indexOf("#access_token=") +14, promise.indexOf("&token_type="));
+			console.log("you did it! token: " +token);
+		}
+	});
+}
+addAccount();
